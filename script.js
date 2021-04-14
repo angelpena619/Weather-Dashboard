aec16c3b73f0cd27415d4282f729606cb30b9bed
Angel <angelpena619@gmail.com>
2021-04-13 18:36:07 -0700

    updating files

--- script.js
+++ script.js
@@ -2,16 +2,37 @@ var apiKey = "939af84008825816b09df6ace4af573b"
 
 var searchBtn = document.getElementById ("search-button");
 searchBtn.addEventListener("click", handleSearch)
-function handleSearch(){
+
+function weatherData(lat,lon,city){
     console.log("click")
-    var lat = 32.733
-    var lon = -117.246
+    
     fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&appid='+apiKey)
   .then(function(response)  {
       return response.json()
      })
-  .then(data => console.log(data));
+  .then(function(data){
+
+  
+    console.log(data)
+   $('#temperature').innerHTML = data.current.temp
+   $('#wind-speed').text = data.current.wind_speed
+   $('#uv-index').text = data.current.uvi
+   $('#humidity').text = data.current.humidity
+
+   
+  }); 
 }
 
 
-
+function handleSearch(){
+  $.ajax({
+    url: `https://api.openweathermap.org/data/2.5/weather?q=seattle&units=imperial&appid=939af84008825816b09df6ace4af573b`,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response)
+    let lat = response.coord.lat;
+    let lon = response.coord.lon;
+    let city = "seattle";
+    weatherData(lat, lon, city);
+  })
+}
