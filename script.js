6216cb23a5fd75534d85ae2eaeebee4cf99ee6c5
Angel <angelpena619@gmail.com>
2021-04-14 17:45:17 -0700

    updating JS file along with index

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var apiKey = "939af84008825816b09df6ace4af573b"
 var searchBtn = document.getElementById ("search-button");
 searchBtn.addEventListener("click", handleSearch)
 
-function weatherData(lat,lon,city){
+function weatherData(lat, lon,city){
     console.log("click")
     
     fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&appid='+apiKey)
@@ -14,11 +14,27 @@ function weatherData(lat,lon,city){
 
   
     console.log(data)
-   $('#temperature').innerHTML = data.current.temp
-   $('#wind-speed').text = data.current.wind_speed
-   $('#uv-index').text = data.current.uvi
-   $('#humidity').text = data.current.humidity
 
+
+  $('#temperature').text(data.current.temp)
+  $('#wind-speed').text(data.current.wind_speed)
+  $('#uv-index').text(data.current.uvi)
+  $('#humidity').text(data.current.humidity + "%")
+
+  $('#futureHumidity0').text(data.daily[0].humidity + "%")
+  $('#futureTemp0').text(data.daily[0].temp.day)
+  
+  $('#futureHumidity1').text(data.daily[1].humidity + "%")
+  $('#futureTemp1').text(data.daily[1].temp.day)
+
+  $('#futureHumidity2').text(data.daily[2].humidity + "%")
+  $('#futureTemp2').text(data.daily[2].temp.day)
+
+  $('#futureHumidity3').text(data.daily[3].humidity + "%")
+  $('#futureTemp3').text(data.daily[3].temp.day)
+
+  $('#futureHumidity4').text(data.daily[4].humidity + "%")
+  $('#futureTemp4').text(data.daily[4].temp.day)
    
   }); 
 }
